4741ee6d8632ce4b7fb2b12922bcf34681501a35
shareef <nadeemshareef934@gmail.com>
2021-05-13 13:52:55 +0530

    Add context-api blog

---
+++ context-api-with-nextjs-and-typescript/README.md
@@ -0,0 +1,4 @@
+# This repo is the code example for my DEV blog
+
+-   [Blog link](https://dev.to/shareef/context-api-with-typescript-and-next-js-2m25)
+-   [Live Demo](https://context-api-with-nextjs-and-ts.vercel.app/)
---
+++ context-api-with-nextjs-and-typescript/pages/_app.js
@@ -0,0 +1,14 @@
+import { AuthProvider } from "../context/AuthContext";
+import "../styles/globals.css";
+
+function MyApp({ Component, pageProps }) {
+    return (
+        <>
+            <AuthProvider>
+                <Component {...pageProps} />
+            </AuthProvider>
+        </>
+    );
+}
+
+export default MyApp;
