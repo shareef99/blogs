9a8ed6339251f23a8fd9388defa653c86b29ff82
shareef <nadeemshareef934@gmail.com>
2021-05-13 13:43:58 +0530

    first

---
+++ next-routing/pages/about.jsx
@@ -0,0 +1,9 @@
+const AboutPage = () => {
+    return (
+        <div>
+            <h1>About page</h1>
+        </div>
+    );
+};
+
+export default AboutPage;
---
+++ next-routing/pages/index.jsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Home() {
+    return (
+        <>
+            <Head></Head>
+            <div>
+                <h1>Home Page</h1>
+            </div>
+        </>
+    );
+}
