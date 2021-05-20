dbc9ad8cfe98d6e47e49b2d9091539774932bef4
shareef <nadeemshareef934@gmail.com>
2021-05-20 21:41:55 +0530

    Completed first topic

--- array-and-usestate/src/App.jsx
+++ array-and-usestate/src/App.jsx
@@ -1,10 +1,46 @@
 import { useState } from "react";
 import "./App.css";
 
+const friendsArray = [
+    {
+        name: "handy",
+        age: 19,
+    },
+    {
+        name: "Candy",
+        age: 18,
+    },
+    {
+        name: "mandy",
+        age: 20,
+    },
+];
+
 const App = () => {
+    const [friends, setFriends] = useState(friendsArray); // Setting default value
+
+    const handleAddFriend = () => {
+        setFriends((prevFriends) => [
+            ...prevFriends,
+            {
+                name: "Random Friend Name",
+                age: 20, // Random age
+            },
+        ]);
+    };
+
     return (
         <main>
             <h1>Hello Developers</h1>
+            <div>
+                {friends.map((friend, index) => (
+                    <li key={index}>
+                        <span>name: {friend.name}</span>{" "}
+                        <span>age: {friend.age}</span>
+                    </li>
+                ))}
+                <button onClick={handleAddFriend}>Add Friends</button>
+            </div>
         </main>
     );
 };
