5aac141f1e50023ed72a44e2493933ea208eb1c0
shareef <nadeemshareef934@gmail.com>
2021-05-20 20:39:02 +0530

    Clear Defaults

---
+++ array-and-usestate/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import "./App.css";
+
+const App = () => {
+    return (
+        <main>
+            <h1>Hello Developers</h1>
+        </main>
+    );
+};
+
+export default App;
