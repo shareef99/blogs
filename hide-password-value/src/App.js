6b38bfa5f5e94d375fac9101b44dff592f29b631
shareef <nadeemshareef934@gmail.com>
2021-12-06 22:04:43 +0530

    🔥 completed blog

--- hide-password-value/src/App.js
+++ hide-password-value/src/App.js
@@ -1,10 +1,38 @@
 import "./App.css";
+import { useFormik } from "formik";
 
 function App() {
+    const handleSubmit = (values) => {
+        console.log(values);
+    };
+
+    const formik = useFormik({
+        initialValues: {
+            email: "",
+            password: "",
+        },
+    });
+
     return (
-        <form>
-            <input name="email" type="email" placeholder="email" />
-            <input name="password" type="password" placeholder="password" />
+        <form
+            onSubmit={(e) => {
+                e.preventDefault();
+                handleSubmit(formik.values);
+            }}
+        >
+            <input
+                name="email"
+                type="email"
+                placeholder="email"
+                {...formik.getFieldProps("email")}
+            />
+            <input
+                name="password"
+                type="password"
+                placeholder="password"
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+            />
             <button type="submit">Submit</button>
         </form>
     );
